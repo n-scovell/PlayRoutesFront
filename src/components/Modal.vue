1bd888686aa8b83c1618221697497bc5d6b46988
n-scovell <N8scovell@gmail.com>
2026-08-16 09:33:50 -0400

    Testing
    Testing for bug

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -116,11 +116,14 @@ const close = () => {
 
 <style lang="scss">
 .mdlBack {
-    position:fixed;
-    top:0;
-    left:0;
-    right:0;
-    bottom:0;
+    // position:fixed;
+    // top:0;
+    // left:0;
+    // right:0;
+    // bottom:0;
+    overflow:hidden;
+    width:50px;
+    height:50px;
     background:rgba(0,0,0,.5);
     backdrop-filter: blur(2px);
     z-index:555;
