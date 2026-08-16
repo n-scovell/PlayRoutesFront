afc676a20f71d171d4fe5bb8499de48f681be4cb
n-scovell <N8scovell@gmail.com>
2026-08-15 20:35:49 -0400

    Updated
    Updated

--- src/views/PlaysView.vue
+++ src/views/PlaysView.vue
@@ -128,7 +128,7 @@ const showMenu = (i: number) => {
 </script>
 <template>
 
-  <Modal :show="showPlay" @close="closePlay" :foo="selectedPlay"/>
+  <!-- <Modal :show="showPlay" @close="closePlay" :foo="selectedPlay"/> -->
 
   <div class="authDelete" v-if="areYouSure">
     <p>Delete:</p>
@@ -139,7 +139,7 @@ const showMenu = (i: number) => {
   
   <main>
     <h1>Your Playbook:</h1>
-    <div>
+    <div v-if="showPlay">
       HELLO ALL
     </div>
     <div class="sorters">
