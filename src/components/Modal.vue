6956f96b1f74af3573cf00a16014c2e627a30bc2
n-scovell <N8scovell@gmail.com>
2026-08-16 13:01:06 -0400

    Up
    Up

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -83,6 +83,10 @@ const close = () => {
     <div v-if="show" class="mdlBack" :class="{active : show}" @click.self="close">
         <div class="mdlCont" ref="captureTarget">
             <button  aria-label="Close Modal" class="mdlClose" @click="close">✕</button>
+            <div class="btCont" v-if="!gst.guest"">
+                <button  aria-label="Download Play" @click="captureDownload(foo?.title)" :disabled="isCapturing">PRINT</button>
+                <button  aria-label="Flip Play" @click="flipPlay()">FLIP</button>
+            </div>
         </div>
         <!-- <div class="mdlCont" ref="captureTarget">
             <button  aria-label="Close Modal" class="mdlClose" @click="close">✕</button>
